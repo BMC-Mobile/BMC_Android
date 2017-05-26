a906ba1d7f1df5f7aa10bcf803b12a12f7955da6
liuyufei <gabriel.liu3615@gmail.com>
2017-05-27 03:47:15 +1200

    loader manager

--- app/src/main/java/com/liuyufei/bmc_android/admin/StaffFragment.java
+++ app/src/main/java/com/liuyufei/bmc_android/admin/StaffFragment.java
@@ -1,25 +1,26 @@
 package com.liuyufei.bmc_android.admin;
 
-import android.content.ContentProvider;
+import android.app.Fragment;
+import android.app.LoaderManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.liuyufei.bmc_android.R;
-import com.liuyufei.bmc_android.admin.dummy.DummyContent;
 import com.liuyufei.bmc_android.admin.dummy.DummyContent.DummyItem;
 import com.liuyufei.bmc_android.data.BMCContract;
+import com.liuyufei.bmc_android.model.Staff;
 
 
 /**
@@ -28,15 +29,13 @@ import com.liuyufei.bmc_android.data.BMCContract;
  * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
  * interface.
  */
-public class StaffFragment extends Fragment {
+public class StaffFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
     private OnListFragmentInteractionListener mListener;
 
     ContentResolver contentResolver;
+    private static final int URL_LOADER = 0;
+
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -50,7 +49,6 @@ public class StaffFragment extends Fragment {
     public static StaffFragment newInstance(int columnCount) {
         StaffFragment fragment = new StaffFragment();
         Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
         fragment.setArguments(args);
         return fragment;
     }
@@ -58,14 +56,14 @@ public class StaffFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
-
+        getLoaderManager().initLoader(URL_LOADER, null, this);
 
     }
 
+
+
+    Cursor cursor;
+    StaffCursorAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -74,9 +72,28 @@ public class StaffFragment extends Fragment {
 
         ListView lv = (ListView) view;
 
-        Cursor cursor = contentResolver.query(BMCContract.StaffEntry.CONTENT_URI, null, null, null, null);
-        StaffCursorAdapter adapter = new StaffCursorAdapter(view.getContext(), cursor, false);
+        adapter = new StaffCursorAdapter(view.getContext(), cursor, false);
         lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                //move the cursor to the selected row
+                cursor = (Cursor) adapterView.getItemAtPosition(pos);
+                //get the object data from the cursor
+                int staffID = cursor.getInt(cursor.getColumnIndex(BMCContract.StaffEntry._ID));
+                String staffName = cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_NAME));
+                String staffPhoto = cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_PHOTO));
+                String staffDepartment = cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_DEPARTMENT));
+                String staffTitle = cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_TITLE));
+                String staffMobile = cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_MOBILE));
+                //create the object that will be passed to the todoActivity
+                Staff staff = new Staff(staffID, staffName, staffPhoto, staffDepartment, staffTitle, staffMobile);
+                Intent intent = new Intent(getActivity(), EditStaffActivity.class);
+                //pass the ID to the todoActivity
+                intent.putExtra("staff", staff);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
@@ -97,6 +114,25 @@ public class StaffFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Loader<Cursor> lc = new CursorLoader(
+                getContext(),
+                BMCContract.StaffEntry.CONTENT_URI, null, null, null, null);
+        return lc;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
     }
 
     /**
