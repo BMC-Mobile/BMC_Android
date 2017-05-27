2bef4774d4aa5ac6256ead73cabb95022275f269
liuyufei <gabriel.liu3615@gmail.com>
2017-05-27 16:51:58 +1200

    read image from gallary

--- app/src/main/java/com/liuyufei/bmc_android/admin/EditStaffActivity.java
+++ app/src/main/java/com/liuyufei/bmc_android/admin/EditStaffActivity.java
@@ -1,17 +1,34 @@
 package com.liuyufei.bmc_android.admin;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.liuyufei.bmc_android.R;
 import com.liuyufei.bmc_android.databinding.ActivityEditStaffBinding;
 import com.liuyufei.bmc_android.model.Staff;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
 public class EditStaffActivity extends AppCompatActivity {
 
     Staff staff;
+
+    int RESULT_LOAD_IMAGE = 1;
+    int REQUEST_PERMISSION=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +40,48 @@ public class EditStaffActivity extends AppCompatActivity {
         ActivityEditStaffBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_edit_staff);
         binding.setStaff(staff);
+
+        findViewById(R.id.staffProfile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                        && ContextCompat.checkSelfPermission(EditStaffActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(EditStaffActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_PERMISSION);
+                    return;
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            } else {
+                // User refused to grant permission.
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            ImageView imageView = (ImageView) findViewById(R.id.staffProfile);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 }
