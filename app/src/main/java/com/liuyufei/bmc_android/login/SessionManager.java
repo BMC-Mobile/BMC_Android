bf7a66ac549178d4c5706e59bc89bcea6e461b33
GoConsult <goingconsult@gmail.com>
2017-06-10 20:59:25 +1200

    Login and shared preference 20170610

---
+++ app/src/main/java/com/liuyufei/bmc_android/login/SessionManager.java
@@ -0,0 +1,82 @@
+package com.liuyufei.bmc_android.login;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.liuyufei.bmc_android.R;
+
+import java.util.HashMap;
+
+/**
+ * Created by YING CHEN on 06/10/17.
+ */
+
+public class SessionManager {
+    private static String TAG="SessionManager";
+    /* Declaration  shared preference */
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    public static final String KEY_NAME="name";
+    public static final String KEY_MOBILE="mobile";
+
+    public SessionManager(Context context) {
+        this._context=context;
+        pref=_context.getSharedPreferences(String.valueOf(R.string.PREF_NAME),R.integer.PRIVATE_MODE);
+        editor=pref.edit();
+    }
+
+    public void createLoginSession(String name, String mobile){
+        /* store preference */
+        editor.putBoolean(String.valueOf(R.string.IsLoggedIn),true);
+        editor.putString(KEY_NAME,name);
+        editor.putString(KEY_MOBILE,mobile);
+        editor.commit();
+    }
+
+    public HashMap<String, String> getUserSession(){
+        HashMap<String, String> user = new HashMap<String, String>();
+        user.put(KEY_NAME, pref.getString(KEY_NAME,null));
+        user.put(KEY_MOBILE,pref.getString(KEY_MOBILE,null));
+        return user;
+    }
+
+    // Get Login State
+    public boolean isLoggedIn(){
+        return pref.getBoolean(String.valueOf(R.string.IsLoggedIn), false);
+    }
+
+    public void logoutUser(){
+        editor.clear();
+        editor.commit();
+        //
+        Log.i(TAG,"Logout");
+    }
+
+    /**
+     * Function to display simple Alert Dialog
+     * @param context -application context
+     * @param title - alert dialog title
+     * @param message - alert message
+     * @param status - true false
+     */
+    public void showAlertDialog(
+            Context context,
+            String title,
+            String message,
+            Boolean status
+    ){
+        // Declare a dialog
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        if(status!=null){
+            alertDialog.setIcon((status)? R.drawable.success:R.drawable.fail);
+        }
+        alertDialog.show();
+    }
+}
