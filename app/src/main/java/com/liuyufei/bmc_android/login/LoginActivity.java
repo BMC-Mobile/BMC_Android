69c73f93b6129fefc15007df5ad928e4301aa9c6
liuyufei <gabriel.liu3615@gmail.com>
2017-06-15 22:56:27 +1200

    change default password to 123456 & pop up mobile doesn't exist warning

--- app/src/main/java/com/liuyufei/bmc_android/login/LoginActivity.java
+++ app/src/main/java/com/liuyufei/bmc_android/login/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.liuyufei.bmc_android.login;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import com.liuyufei.bmc_android.R;
+
 import android.content.AsyncQueryHandler;
 import android.content.Intent;
 import android.database.Cursor;
@@ -13,16 +16,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.liuyufei.bmc_android.MainActivity;
+import com.liuyufei.bmc_android.admin.AdminActivity;
 import com.liuyufei.bmc_android.data.BMCContract;
 import com.liuyufei.bmc_android.model.Staff;
 
 /**
- *  Created by Ying Chen 20170610
- * */
-public class LoginActivity extends AppCompatActivity{
+ * Created by Ying Chen 20170610
+ */
+public class LoginActivity extends AppCompatActivity {
 
-    static String DEFAULT_PASSWORD="1234";
+    static String DEFAULT_PASSWORD = "123456";
     static String TAG = "LoginActivity";
     // Email, password edittext
     EditText txtUsername, txtPassword;
@@ -35,6 +40,7 @@ public class LoginActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
         // Session Manager
         session = new SessionManager(getApplicationContext());
@@ -50,7 +56,7 @@ public class LoginActivity extends AppCompatActivity{
         btnRing = (Button) findViewById(R.id.ring_button);
 
         // Set click Listener
-        btnLogin.setOnClickListener(new View.OnClickListener(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Get username, password from EditText
@@ -58,22 +64,22 @@ public class LoginActivity extends AppCompatActivity{
                 String password = txtPassword.getText().toString();
 
                 // Check if username, password is filled
-                if(username.trim().length() > 0 && password.trim().length() > 0){
-                    _mobile=username;
-                    _password=password;
+                if (username.trim().length() > 0 && password.trim().length() > 0) {
+                    _mobile = username;
+                    _password = password;
                     userLogin();
-                }else{
-                    Log.i(TAG,"password is empty");
+                } else {
+                    Log.i(TAG, "password is empty");
                     session.showAlertDialog(LoginActivity.this, "Login failed..", "Please enter username and password", false);
                 }
             }
         });
 
         // Set click Ring
-        btnRing.setOnClickListener(new View.OnClickListener(){
+        btnRing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int taux_max=200;
+                int taux_max = 200;
                 int val = 200;
                 ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 50);
                 btnRing.setTextColor(getResources().getColor(R.color.colorButtonFont));
@@ -84,34 +90,32 @@ public class LoginActivity extends AppCompatActivity{
     String _mobile;
     String _password;
 
-    private void userLogin(){
+    private void userLogin() {
 
-        AsyncQueryHandler queryHandler = new AsyncQueryHandler(getContentResolver()){
+        AsyncQueryHandler queryHandler = new AsyncQueryHandler(getContentResolver()) {
             @Override
             protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
-                Staff staff = new Staff(0,null,null,null,null,null);
-                try{
-                    if(cursor.moveToNext()){
-                        Integer id = cursor.getInt(cursor.getColumnIndex(BMCContract.StaffEntry._ID));
+                try {
+                    if (cursor.moveToNext()) {
                         String name = cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_NAME));
                         String mobileNumber = cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_NAME));
-                        String department=cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_DEPARTMENT));
-                        String photo=cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_PHOTO));
-                        String title=cursor.getString(cursor.getColumnIndex(BMCContract.StaffEntry.COLUMN_TITLE));
 
-                        if (name.trim().length()>0&&_mobile.trim().length()>0){
+                        if (name.trim().length() > 0 && _mobile.trim().length() > 0) {
                             // create login session
-                            if(_password.trim().equals(DEFAULT_PASSWORD)){
-                                session.createLoginSession(name,mobileNumber);
-                                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                            if (_password.trim().equals(DEFAULT_PASSWORD)) {
+                                session.createLoginSession(name, mobileNumber);
+                                Intent i = new Intent(getApplicationContext(), AdminActivity.class);
                                 startActivity(i);
                                 finish();
-                            }else {
-                                Log.i(TAG,"password doesn't match");
+                            } else {
+                                Log.i(TAG, "password doesn't match");
                                 session.showAlertDialog(LoginActivity.this, "Login failed..", "Username/Password is incorrect", false);
                             }
                         }
+                    } else {
+                        session.showAlertDialog(LoginActivity.this, "Login failed..", "Input mobile:" + _mobile + " doesn't exist", false);
                     }
+
                 } finally {
                     cursor.close();
                 }
@@ -119,7 +123,7 @@ public class LoginActivity extends AppCompatActivity{
         };
         String selection = BMCContract.StaffEntry.COLUMN_MOBILE + "=?";
         String[] args = {_mobile};
-        String orderBy = BMCContract.StaffEntry.COLUMN_MOBILE+" DESC";
+        String orderBy = BMCContract.StaffEntry.COLUMN_MOBILE + " DESC";
 
         // query data when it is done, call on query complete
         queryHandler.startQuery(0, null, BMCContract.StaffEntry.CONTENT_URI, null, selection, args, orderBy);
