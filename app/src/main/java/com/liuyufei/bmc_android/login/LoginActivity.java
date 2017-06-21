724df40c031e725717d0be716f19ec4e287afa89
yingchennz <cydirac@gmail.com>
2017-06-21 15:39:30 +1200

    user checkin ui

--- app/src/main/java/com/liuyufei/bmc_android/login/LoginActivity.java
+++ app/src/main/java/com/liuyufei/bmc_android/login/LoginActivity.java
@@ -1,18 +1,14 @@
 package com.liuyufei.bmc_android.login;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.liuyufei.bmc_android.R;
-
 import android.content.AsyncQueryHandler;
 import android.content.Intent;
 import android.database.Cursor;
@@ -20,7 +16,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.liuyufei.bmc_android.admin.AdminActivity;
